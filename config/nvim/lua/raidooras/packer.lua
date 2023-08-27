a90558d104bc5e6bdd948d1898606f375f3692a2
Raido Oras <oras.raido@gmail.com>
2023-08-28 01:25:12 +0300

    Clean up packer.lua

--- config/nvim/lua/raidooras/packer.lua
+++ config/nvim/lua/raidooras/packer.lua
@@ -1,20 +1,15 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
-  -- Telescope
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use "EdenEast/nightfox.nvim" -- Packer
+  use "EdenEast/nightfox.nvim"
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
